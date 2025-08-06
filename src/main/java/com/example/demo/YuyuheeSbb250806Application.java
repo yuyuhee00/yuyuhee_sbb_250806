79334b8f0a0811073f351f2217dad8dcfdd6f1a9
yuyuhee00 <yuyuhee00@gmail.com>
2025-08-06 14:30:38 +0900

    init

---
+++ src/main/java/com/example/demo/YuyuheeSbb250806Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuyuheeSbb250806Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuyuheeSbb250806Application.class, args);
+    }
+
+}
